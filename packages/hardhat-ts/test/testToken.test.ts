1138507d91288ea041fd89ca1b2e5217633af47a
Andy <admazzola@gmail.com>
2022-03-04 19:49:06 -0500

    test passing

--- packages/hardhat-ts/test/testToken.test.ts
+++ packages/hardhat-ts/test/testToken.test.ts
@@ -1,33 +1,57 @@
-import { Contract, Signer, BigNumber as BN } from 'ethers'
-import * as hre from 'hardhat'
+
+import { BigNumber as BN, BigNumberish, Signer } from 'ethers'
+import hre from 'hardhat'
 import { XTestToken  } from '../types/typechain'
 import { use, should } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import { solidity } from 'ethereum-waffle'
-import { deploy } from '../utils/deploy-helpers'
-import {setup} from './helpers/setup'
+ 
 
 use(chaiAsPromised)
 use(solidity)
 should()
  
+const { getNamedSigner, getNamedAccounts, ethers, toBN, deployments, tokens } =
+  hre
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface SetupOptions {}
+
+interface SetupReturn {
+  testToken: XTestToken
+}
+
+
+const setup = deployments.createFixture<SetupReturn, SetupOptions>(
+  async (hre, _opts) => {
+    await hre.deployments.fixture(['primary'], {
+      keepExistingDeployments: false,
+    })
+
+    const testToken = await hre.contracts.get<XTestToken>('_0xTestToken')
+   
+
+    return {
+      testToken
+    }
+  }
+)
+
 
 describe('0xTestToken', function () {
   let testTokenContract: XTestToken
   
-  let user: Signer
-  let filler: Signer
-   
-
-  beforeEach(async () => {
-    const result = await setup()
-    testTokenContract = result._0xTestToken
+  let deployer: Signer
     
-    user = result.user
+
+  before(async () => {
+    const result = await setup()
+    testTokenContract = result.testToken
+    
+    deployer = await getNamedSigner('deployer')
    
   })
-
-    
+  
   
 
   describe('0xTestToken', () => {
